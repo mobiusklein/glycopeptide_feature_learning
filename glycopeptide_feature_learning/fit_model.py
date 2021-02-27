Fix missing offset calculation in AminoAcidsNeighboringModel

glycopeptide_feature_learning/fit_model.py
@@ -4,6 +4,7 @@ import glob
 import json
 import logging
 import warnings
+import array
 try:
     import cPickle as pickle
 except ImportError:
@@ -37,7 +38,7 @@ def get_training_data(paths, blacklist_path=None, threshold=50.0):
     for path in paths:
         training_files.extend(glob.glob(path))
     if len(training_files) == 0:
-        raise click.ClickException("No training files found for patterns {}".format(', '.join(paths)))
+        raise click.ClickException("No spectrum match files found for patterns {}".format(', '.join(paths)))
     training_instances = []
     if blacklist_path is not None:
         with open(blacklist_path) as fh:
@@ -48,7 +49,7 @@ def get_training_data(paths, blacklist_path=None, threshold=50.0):
     seen = set()
     n_files = len(training_files)
     progbar = click.progressbar(
-        training_files, length=n_files, show_eta=True, label='Loading Training Data',
+        training_files, length=n_files, show_eta=True, label='Loading GPSM Data',
         item_show_func=lambda x: os.path.basename(x) if x is not None else '')
     with progbar:
         for train_file in progbar:
@@ -85,8 +86,9 @@ def match_spectra(matches, error_tolerance):
     return matches
 
 
-def partition_training_data(training_instances):
-    partition_map = partitions.partition_observations(training_instances)
+def partition_training_data(training_instances, omit_labile=False):
+    partition_map = partitions.partition_observations(
+        training_instances, omit_labile=omit_labile)
     return partition_map
 
 
@@ -229,10 +231,13 @@ def _fit_model_inner(spec, cell, regression_model, use_mixture=True, **kwargs):
     fits = [fit]
     if use_mixture:
         mismatches = []
+        corr = array.array('d')
         for case in cell.subset:
             r = fit.calculate_correlation(case)
             if r < 0.5:
                 mismatches.append(case)
+            corr.append(r)
+        click.echo("Median Correlation: %0.3f" % np.nanmedian(corr))
         if mismatches:
             try:
                 click.echo("Fitting Mismatch Model with %d cases" % len(mismatches))
@@ -240,9 +245,12 @@ def _fit_model_inner(spec, cell, regression_model, use_mixture=True, **kwargs):
                     mismatch_fit = regression_model.fit_regression(
                         mismatches, reliability_model=fm, base_reliability=0.5, **kwargs)
                     if np.isinf(mismatch_fit.estimate_dispersion()):
+                        click.echo("Infinite dispersion, refitting without per-fragment weights")
                         mismatch_fit = regression_model.fit_regression(
                             mismatches, reliability_model=None, **kwargs)
                 except ValueError:
+                    click.echo(
+                        "%r, refitting without per-fragment weights" % (ex, ))
                     mismatch_fit = regression_model.fit_regression(
                         mismatches, reliability_model=None, **kwargs)
                 mismatch_fit.reliability_model = fm
@@ -263,25 +271,35 @@ def _fit_model_inner(spec, cell, regression_model, use_mixture=True, **kwargs):
               help=('Include the intermediary results and statistics for each model fit, '
                     'allowing the result to be used to describe the model parameters but at the cost of '
                     'greatly increasing the size of the model output file'))
+@click.option("-b", "--omit-labile", is_flag=True, help="Do not include labile monosaccharides when partitioning glycan compositions")
 @click.option("--debug", is_flag=True, default=False, help='Enable debug logging')
-def main(paths, threshold=50.0, output_path=None, blacklist_path=None, error_tolerance=2e-5, debug=False, save_fit_statistics=False):
+def main(paths, threshold=50.0, output_path=None, blacklist_path=None, error_tolerance=2e-5, debug=False, save_fit_statistics=False,
+         omit_labile=False, regression_model=None):
     logger = logging.getLogger()
     if debug:
         logger.setLevel("DEBUG")
     else:
         logger.setLevel("INFO")
     logger.addHandler(logging.StreamHandler(sys.stdout))
+
+    if regression_model is None:
+        if omit_labile:
+            regression_model = multinomial_regression.StubChargeModelApproximate
+        else:
+            regression_model = multinomial_regression.StubChargeModel
+    logger.info("Model Type: %r" % (regression_model, ))
     click.echo("Loading data from %s" % (', '.join(paths)))
     training_instances = get_training_data(paths, blacklist_path, threshold)
     if len(training_instances) == 0:
         raise click.ClickException("No training examples were found.")
     match_spectra(training_instances, error_tolerance)
     click.echo("Partitioning %d instances" % (len(training_instances), ))
-    partition_map = partition_training_data(training_instances)
+    partition_map = partition_training_data(
+        training_instances, omit_labile=omit_labile)
     click.echo("Fitting Peak Relation Features")
     fit_peak_relation_features(partition_map)
     click.echo("Fitting Peak Intensity Regression")
-    model_fits = fit_regression_model(partition_map, trace=debug)
+    model_fits = fit_regression_model(partition_map, regression_model=regression_model, trace=debug)
     click.echo("Writing Models To %s" % (output_path,))
     export = []
     for spec, fit in model_fits:
@@ -292,7 +310,7 @@ def main(paths, threshold=50.0, output_path=None, blacklist_path=None, error_tol
 
 @click.command("partition-glycopeptide-training-data", short_help="Pre-separate training data along partitions")
 @click.option('-t', '--threshold', type=float, default=0.0)
-@click.argument('paths', metavar='PATH', type=click.Path(exists=True, dir_okay=False), nargs=-1)
+@click.argument('paths', metavar='PATH', nargs=-1)
 @click.argument('outdir', metavar='OUTDIR', type=click.Path(dir_okay=True, file_okay=False), nargs=1)
 def partition_glycopeptide_training_data(paths, outdir, threshold=50.0, output_path=None, blacklist_path=None, error_tolerance=2e-5):
     click.echo("Loading data from %s" % (', '.join(paths)))
@@ -335,6 +353,65 @@ def compile_model(inpath, outpath, model_type="partial-peptide"):
     stream.close()
 
 
+
+@click.command("calculate-correlation")
+@click.argument('paths', metavar='PATH', nargs=-1)
+@click.argument("outpath", type=click.Path(dir_okay=False, writable=True))
+@click.argument("model_path", type=click.Path(exists=True, dir_okay=False))
+@click.option('-t', '--threshold', type=float, default=0.0)
+def calculate_correlation(paths, model_path, outpath, threshold=0.0, error_tolerance=2e-5):
+    training_instances = get_training_data(paths, threshold=threshold)
+    model_tree = None
+
+    with open(model_path, 'rb') as fh:
+        model_tree = pickle.load(fh)
+
+    correlations = array.array('d')
+    glycan_correlations = array.array('d')
+    peptide_correlations = array.array('d')
+    scan_ids = []
+
+    def peptide_correlation(match):
+        c, inten, t, y, rel = match.get_predicted_intensities_series(
+            ['b', 'y'], True)
+        return np.corrcoef(inten, y)[1, 0]
+
+    def glycan_correlation(match):
+        c, inten, t, y, rel = match.get_predicted_intensities_series(
+            ['stub_glycopeptide'], True)
+        return np.corrcoef(inten, y)[1, 0]
+
+    progbar = click.progressbar(
+        enumerate(training_instances), length=len(training_instances), show_eta=True, label='Matching Peaks',
+
+        item_show_func=lambda x: "%d Spectra Matched" % (x[0],) if x is not None else '')
+    with progbar:
+        for i, scan in progbar:
+            match = model_tree.evaluate(scan, scan.structure, error_tolerance=error_tolerance)
+            if progbar.is_hidden and i % 1000 == 0 and i != 0:
+                click.echo("%d Spectra Matched" % (i,))
+
+            correlations.append(match._mixture_apply(match._calculate_correlation_coef))
+            peptide_correlations.append(match._mixture_apply(peptide_correlation))
+            glycan_correlations.append(match._mixture_apply(glycan_correlation))
+            scan_ids.append(scan.id)
+
+    if not progbar.is_hidden:
+        click.echo("%d Spectra Matched" % (i,))
+
+    click.echo("Median Correlation: %03f" % np.nanmedian(correlations))
+    click.echo("Median Glycan Correlation: %03f" % np.nanmedian(glycan_correlations))
+    click.echo("Median Peptide Correlation: %03f" % np.nanmedian(peptide_correlations))
+
+    with open(outpath, 'wb') as fh:
+        pickle.dump({
+            "scan_id": scan_ids,
+            "correlation": correlations,
+            "peptide_correlation": peptide_correlations,
+            "glycan_correlation": glycan_correlations,
+        }, fh)
+
+
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
@@ -347,6 +424,7 @@ cli.add_command(main, "fit-model")
 cli.add_command(partition_glycopeptide_training_data, "partition-samples")
 cli.add_command(strip_model_arrays, "strip-model")
 cli.add_command(compile_model, "compile-model")
+cli.add_command(calculate_correlation, 'calculate-correlation')
 
 
 def info(type, value, tb):

glycopeptide_feature_learning/multinomial_regression.py
@@ -68,6 +68,9 @@ BackboneFragment_max_glycosylation_size = 2
 FragmentCharge_max = 4
 # consider up to 10 monosaccharides of glycan still attached to a stub ion
 StubFragment_max_glycosylation_size = 10
+# consider up to 6 labile monosaccharide losses from an intact glycan
+StubFragment_max_labile_monosaccharides = 6
+
 
 _FragmentType = namedtuple(
     "FragmentType", [
@@ -267,6 +270,7 @@ class FragmentType(_FragmentType):
         matched_total = 0
         total = sum(p.intensity for p in gpsm.deconvoluted_peak_set)
         counted = set()
+        target = gpsm.target
         if gpsm.solution_map is None:
             gpsm.match()
         for peak_fragment_pair in gpsm.solution_map:
@@ -283,9 +287,9 @@ class FragmentType(_FragmentType):
                     cls(
                         None, None, FragmentSeriesClassification.stub_glycopeptide,
                         min(fragment.glycosylation_size, StubFragment_max_glycosylation_size),
-                        min(peak.charge, FragmentCharge_max + 1), peak_fragment_pair, gpsm.structure))
+                        min(peak.charge, FragmentCharge_max + 1), peak_fragment_pair, target))
                 continue
-            inst = cls.from_peak_peptide_fragment_pair(peak_fragment_pair, gpsm.structure)
+            inst = cls.from_peak_peptide_fragment_pair(peak_fragment_pair, target)
             fragment_classification.append(inst)
 
         unassigned = total - matched_total
@@ -322,7 +326,7 @@ class FragmentType(_FragmentType):
                 for i, frag_spec in enumerate(c):
                     peak_pair = frag_spec.peak_pair
                     if peak_pair is None:
-                        reliability[i] = 1.0
+                        reliability[i] = base_reliability
                     else:
                         reliability[i] = (remaining_reliability * reliability_score_map[peak_pair]
                                           ) + base_reliability
@@ -330,9 +334,9 @@ class FragmentType(_FragmentType):
             reliabilities.append(reliability)
         try:
             fit = multinomial_fit(breaks, matched, totals, reliabilities=reliabilities, **kwargs)
-        except np.linalg.LinAlgError:
-            logger.debug(
-                "Fitting the model with per-fragment reliability information failed.", exc_info=True)
+        except np.linalg.LinAlgError as err:
+            logger.info(
+                "Fitting the model with per-fragment reliability information failed: %s", err)
             fit = multinomial_fit(breaks, matched, totals, reliabilities=None, **kwargs)
         return MultinomialRegressionFit(model_type=cls, reliability_model=reliability_model, **fit)
 
@@ -551,6 +555,8 @@ class NeighboringAminoAcidsModel(StubGlycopeptideFucosylationModel):
                 if cterm is not None:
                     X[offset + cterm.value] = 1
                 offset += k_ftypes
+        else:
+            offset += (k_ftypes * self.bond_offset_depth * 2)
         return X, offset
 
     def build_feature_vector(self, X, offset):
@@ -699,6 +705,101 @@ try:
     StubChargeModel.build_feature_vector = StubChargeModel_build_feature_vector
 except ImportError as err:
     print(err)
+
+
+class StubChargeModelApproximate(StubChargeModel):
+    def build_feature_vector(self, X, offset):
+        # X, offset = super(StubChargeModel, self).build_feature_vector(X, offset)
+        # X, offset = self.encode_stub_charge(X, offset)
+
+        # Directly invoke feature vector construction because super() costs too much
+        # in a tight loop
+        X, offset = FragmentType.build_feature_vector(self, X, offset)
+        X, offset = ProlineSpecializingModel.specialize_proline(self, X, offset)
+        X, offset = StubGlycopeptideCompositionModel.encode_stub_information(self, X, offset)
+        X, offset = StubGlycopeptideFucosylationModel.encode_stub_fucosylation(self, X, offset)
+        X, offset = NeighboringAminoAcidsModelDepth2.encode_neighboring_residues(self, X, offset)
+        X, offset = StubChargeModelApproximate.encode_stub_charge_approximate(
+            self, X, offset)
+        return X, offset
+
+    def encode_stub_charge_approximate(self, X, offset):
+        k_glycosylated_stubs = (StubFragment_max_glycosylation_size * 2) + 1
+        k_stub_charges = FragmentCharge_max + 1
+        k_glycosylated_stubs_x_charge = (k_glycosylated_stubs * k_stub_charges)
+
+        if self.is_stub_glycopeptide():
+            # TODO: Using the approximation provides a mildly better model fit on mixed sialylated/non-sialylated data
+            # but requires a full re-analysis. Save for the future.
+            loss_size = approximate_internal_size_of_glycan(self.sequence.glycan_composition) - int(self.glycosylated)
+            # loss_size = sum(
+            #     self.sequence.glycan_composition.values()) - int(self.glycosylated)
+            if loss_size >= k_glycosylated_stubs:
+                loss_size = k_glycosylated_stubs - 1
+            # d = k_glycosylated_stubs * (self.charge - 1) + int(self.glycosylated)
+            d = k_glycosylated_stubs * (self.charge - 1) + loss_size
+            X[offset + d] = 1
+        offset += k_glycosylated_stubs_x_charge
+        return X, offset
+
+
+_NEUAC = FrozenMonosaccharideResidue.from_iupac_lite("NeuAc")
+_NEUGC = FrozenMonosaccharideResidue.from_iupac_lite("NeuGc")
+
+
+def count_labile_monosaccharides(glycan_composition):
+    k = glycan_composition._getitem_fast(_NEUAC)
+    k += glycan_composition._getitem_fast(_NEUGC)
+    return k
+
+
+class LabileMonosaccharideAwareModel(StubChargeModel):
+
+    @classmethod
+    def feature_names(cls):
+        names = super(LabileMonosaccharideAwareModel, cls).feature_names()
+        k_labile_monosaccharides = (
+            StubFragment_max_labile_monosaccharides) + 1
+        k_stub_charges = FragmentCharge_max + 1
+        for i in range(k_stub_charges):
+            for j in range(k_labile_monosaccharides):
+                names.append(
+                    "stub glycopeptide:charge %d:labile monosaccharides %d" % (i + 1, j))
+        return names
+
+    def encode_labile_monosaccharides_charge(self, X, offset):
+        k_labile_monosaccharides = (StubFragment_max_labile_monosaccharides) + 1
+        k_stub_charges = FragmentCharge_max + 1
+        k_labile_monosaccharides_x_charge = (k_labile_monosaccharides * k_stub_charges)
+
+        if self.is_stub_glycopeptide():
+            loss_size = count_labile_monosaccharides(
+                self.sequence.glycan_composition)
+            if loss_size >= k_labile_monosaccharides:
+                loss_size = k_labile_monosaccharides - 1
+            d = k_labile_monosaccharides * (self.charge - 1) + loss_size
+            X[offset + d] = 1
+        offset += k_labile_monosaccharides_x_charge
+        return X, offset
+
+    def build_feature_vector(self, X, offset):
+        # X, offset = super(StubChargeModel, self).build_feature_vector(X, offset)
+        # X, offset = self.encode_stub_charge(X, offset)
+
+        # Directly invoke feature vector construction because super() costs too much
+        # in a tight loop
+        X, offset = FragmentType.build_feature_vector(self, X, offset)
+        X, offset = ProlineSpecializingModel.specialize_proline(
+            self, X, offset)
+        X, offset = StubGlycopeptideCompositionModel.encode_stub_information(
+            self, X, offset)
+        X, offset = StubGlycopeptideFucosylationModel.encode_stub_fucosylation(
+            self, X, offset)
+        X, offset = NeighboringAminoAcidsModelDepth2.encode_neighboring_residues(
+            self, X, offset)
+        X, offset = StubChargeModel.encode_stub_charge(self, X, offset)
+        X, offset = LabileMonosaccharideAwareModel.encode_labile_monosaccharides_charge(self, X, offset)
+        return X, offset
 
 
 class AmideBondCrossproductModel(StubGlycopeptideCompositionModel):
@@ -904,7 +1005,7 @@ def multinomial_fit(x, y, weights, reliabilities=None, dispersion=1, adjust_disp
         z = phi * beta0
         H = phi * np.diag(S_inv0)
         logger.info(
-            "Iteration %d\n", iter_)
+            "Iteration %d (%0.2e)", iter_, dev)
         # if tracing:
         #     assert not np.any(np.isnan(H))
         for i in range(len(y)):