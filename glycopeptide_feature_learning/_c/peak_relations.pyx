966a78615de125e79cd2c4dd0be1ed71a1257c07
Joshua Klein <mobiusklein@gmail.com>
2022-03-22 07:49:17 -0400

    Re-un-revert peak_key

--- glycopeptide_feature_learning/_c/peak_relations.pyx
+++ glycopeptide_feature_learning/_c/peak_relations.pyx
@@ -776,9 +776,9 @@ cdef class PeakRelation(object):
 
     cpdef tuple peak_key(self):
         if self.from_peak._index.neutral_mass < self.to_peak._index.neutral_mass:
-            return self.from_peak, self.to_peak
+            return self.from_peak._index.neutral_mass, self.to_peak._index.neutral_mass
         else:
-            return self.to_peak, self.from_peak
+            return self.to_peak._index.neutral_mass, self.from_peak._index.neutral_mass
 
     @staticmethod
     cdef PeakRelation _create(DeconvolutedPeak from_peak, DeconvolutedPeak to_peak, feature, IonSeriesBase series):
